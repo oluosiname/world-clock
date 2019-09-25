957c772e033711129ecf9017c1da7aeb17cbad56
Olumuyiwa <oluosiname@gmail.com>
2019-09-25 18:05:38 +0200

    add timezone selection

--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
 function setTime() {
-  const now = new Date();
+  let timezone = document.querySelector("#timezone-form-selector");
+  let timezoneValue = timezone.options[timezone.selectedIndex].value;
+
+  console.log(timezoneValue);
+
+  let now;
+
+  if (timezoneValue !== "") {
+    now = new Date(
+      new Date().toLocaleString("en-US", {
+        timeZone: timezoneValue
+      })
+    );
+  } else {
+    now = new Date();
+  }
+
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
-  const secondsDegree = ((seconds / 60) * 360) + 90;
-  const minutesDegree = ((minutes / 60) * 360) + 90;
-  const hoursDegree = ((hours / 12) * 360) + 90;
+  const secondsDegree = (seconds / 60) * 360 + 90;
+  const minutesDegree = (minutes / 60) * 360 + 90;
+  const hoursDegree = (hours / 12) * 360 + 90;
 
-  const secondHand = document.querySelector('.second-hand');
-  secondHand.style.transform = `rotate(${secondsDegree}deg)`
+  const secondHand = document.querySelector(".second-hand");
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
-  const minuteHand = document.querySelector('.min-hand');
+  const minuteHand = document.querySelector(".min-hand");
   minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
-  const hourHand = document.querySelector('.hour-hand');
+  const hourHand = document.querySelector(".hour-hand");
   hourHand.style.transform = `rotate(${hoursDegree}deg)`;
   // let seconds = now.getDate('seconds');
   // let seconds = now.getDate('seconds');
 }
 
-setInterval(setTime, 1000)
+setInterval(setTime, 1000);
